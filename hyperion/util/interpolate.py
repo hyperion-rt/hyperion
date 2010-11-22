7d576a7a3362991efbef900b3d4c11382582262e
astrofrog <thomas.robitaille@gmail.com>
2010-11-22 22:07:48 +0000

    Added bounds checking for interpolation routines (using a decorator)

--- hyperion/util/interpolate.py
+++ hyperion/util/interpolate.py
@@ -1,6 +1,45 @@
 import numpy as np
 
 
+class check_bounds(object):
+    '''
+    Decorator to add standard interpolation bounds checking.
+
+    This decorator incurs an overhead of 30-50 percent on the runtime of
+    the interpolation routine.
+    '''
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, x, y, xval, bounds_error=True, fill_value=np.nan):
+        if np.isscalar(xval):  # xval is a scalar
+            if xval < x[0] or xval > x[-1]:  # the value is out of bounds
+                if bounds_error:
+                    raise Exception("x value is out of interpolation bounds")
+                else:
+                    return np.nan
+            else:  # the value is in the bounds
+                return self.f(x, y, xval)
+        else:  # xval is an array
+            inside = (xval >= x[0]) & (xval <= x[-1])
+            outside = ~inside
+            if np.any(outside):  # some values are out of bounds
+                if bounds_error:
+                    raise Exception("x values are out of interpolation bounds")
+                else:
+                    if np.any(inside):
+                        yval = np.zeros(xval.shape)
+                        yval[inside] = self.f(x, y, xval[inside])
+                        yval[outside] = fill_value
+                        return yval
+                    else:
+                        return np.repeat(fill_value, xval.shape)
+            else:  # all values are in the bounds
+                return self.f(x, y, xval)
+
+
+@check_bounds
 def interp1d_fast(x, y, xval):
     '''On-the-fly linear interpolator'''
     ipos = np.searchsorted(x, xval)
@@ -10,6 +49,7 @@ def interp1d_fast(x, y, xval):
          + y[ipos - 1]
 
 
+@check_bounds
 def interp1d_fast_loglog(x, y, xval):
     '''On-the-fly log interpolator'''
     ipos = np.searchsorted(x, xval)
@@ -25,6 +65,7 @@ def interp1d_fast_loglog(x, y, xval):
     return yval
 
 
+@check_bounds
 def interp1d_fast_linlog(x, y, xval):
     '''On-the-fly linear-log interpolator'''
     ipos = np.searchsorted(x, xval)
@@ -40,6 +81,7 @@ def interp1d_fast_linlog(x, y, xval):
     return yval
 
 
+@check_bounds
 def interp1d_fast_loglin(x, y, xval):
     '''On-the-fly log-linear interpolator'''
     ipos = np.searchsorted(x, xval)
