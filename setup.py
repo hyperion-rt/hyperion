c24853b7e443b253b9c8efa8418024260d45794c
astrofrog <thomas.robitaille@gmail.com>
2010-11-27 03:28:49 +0000

    0.6.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 scripts = ['ttsre2rtin', 'ttsre2rtin_auto', 'hyperion']
 
 setup(name='hyperion',
-      version='0.6.3',
+      version='0.6.4',
       packages=['hyperion', 'hyperion.model', 'hyperion.conf', 'hyperion.densities',
                 'hyperion.dust', 'hyperion.util', 'hyperion.atmos',
                 'hyperion.grid', 'hyperion.sources'],
