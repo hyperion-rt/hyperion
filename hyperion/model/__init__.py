474ab78b4707ae619c6b8f7f53899f3c8affd933
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-03-20 22:42:39 +0100

    Import ModelOutput into main namespace

--- hyperion/model/__init__.py
+++ hyperion/model/__init__.py
@@ -1,3 +1,3 @@
-from model import Model, find_last_iteration
+from model import Model, ModelOutput, find_last_iteration
 from analytical_yso_model import AnalyticalYSOModel
 from ttsre_model import TtsreModel
