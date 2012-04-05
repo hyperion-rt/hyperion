93235ce3008a217bcbe10fc7bbed3e9d08c4d003
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-04-05 22:46:39 +0200

    Added basic Model test

---
+++ hyperion/model/tests/test_model.py
@@ -0,0 +1,10 @@
+from .. import Model
+from test_helpers import random_filename
+
+
+def test_basic():
+
+    m = Model()
+    m.set_cartesian_grid([-1., 1.], [-1., 1.], [-1., 1.])
+    m.set_n_photons(initial=100, imaging=100)
+    m.write(random_filename())
