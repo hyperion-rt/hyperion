29ce5c788c56d4c6074891d17862afdddca0f514
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-04-15 17:18:29 +0200

    Include tests in build

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ scripts = ['ttsre2rtin', 'ttsre2rtin_auto', 'hyperion', 'hyperion2fits', 'mcther
 
 setup(name='hyperion',
       version='0.8.4',
-      packages=['hyperion', 'hyperion.model', 'hyperion.conf', 'hyperion.densities',
-                'hyperion.dust', 'hyperion.util', 'hyperion.atmos',
-                'hyperion.grid', 'hyperion.sources', 'hyperion.importers'],
+      packages=['hyperion', 'hyperion.model', 'hyperion.model.tests',
+                'hyperion.conf', 'hyperion.densities',
+                'hyperion.densities.tests', 'hyperion.dust', 'hyperion.util',
+                'hyperion.util.tests', 'hyperion.atmos', 'hyperion.grid',
+                'hyperion.grid.tests', 'hyperion.sources',
+                'hyperion.sources.tests', 'hyperion.importers'],
       scripts=['scripts/' + x for x in scripts])
