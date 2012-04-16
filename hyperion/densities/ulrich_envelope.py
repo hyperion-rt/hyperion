fc01ff434286a7f40cbdbbfa47e3d291c34891ec
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-04-16 15:33:07 +0200

    Fix integrals for cumulative midplane density for Ulrich envelopes. When integrating over r, and changing to integral over gamma, there should be a multiplicative R_c term in front on the integral. Fixes #16.

--- hyperion/densities/ulrich_envelope.py
+++ hyperion/densities/ulrich_envelope.py
@@ -254,7 +254,7 @@ class UlrichEnvelope(Envelope):
 
         if gamma_0 < 1.:
 
-            rho[gamma_1 < 1.] = self.rho_0 \
+            rho[gamma_1 < 1.] = self.rho_0 * self.rc \
                               * (np.log((np.sqrt(gamma_1) + 1) / (1. - np.sqrt(gamma_1))) \
                               - np.log((np.sqrt(gamma_0) + 1) / (1. - np.sqrt(gamma_0))))
 
@@ -262,7 +262,7 @@ class UlrichEnvelope(Envelope):
 
         elif gamma_0 > 1:
 
-            rho[:] = self.rho_0 \
+            rho[:] = self.rho_0 * self.rc \
                    * (np.log((np.sqrt(2. * gamma_1 - 1.) - 1.) / (np.sqrt(2. * gamma_1 - 1.) + 1.)) \
                    - np.log((np.sqrt(2. * gamma_0 - 1.) - 1.) / (np.sqrt(2. * gamma_0 - 1.) + 1.)))
 
