6f5d1b33494d67435a3dbffcaa0bcd5dd0a0653c
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-04-26 15:34:49 +0200

    Added test for optically thin radius in AnalyticalYSOModel

--- hyperion/model/tests/test_analytical_yso.py
+++ hyperion/model/tests/test_analytical_yso.py
@@ -6,7 +6,7 @@ from .. import AnalyticalYSOModel
 from ...util.constants import msun, rsun, lsun, tsun, au, pc
 from ...util.functions import random_filename
 from .test_helpers import get_test_dust
-
+from ...util.convenience import OptThinRadius
 
 def test_full_analytical_yso():
 
@@ -49,3 +49,28 @@ def test_nostar_invalid():
     with pytest.raises(Exception) as e:
         m.set_spherical_polar_grid_auto(1, 1, 1)
     assert e.value.args[0] == 'The central source radius need to be defined before the grid can be set up'
+
+
+def test_analytical_yso_optthinradius():
+
+    dust = get_test_dust()
+
+    m = AnalyticalYSOModel()
+
+    m.star.radius = rsun
+    m.star.temperature = tsun
+    m.star.luminosity = lsun
+
+    e = m.add_power_law_envelope()
+    e.mass = 1.e-2 * msun
+    e.rmin = OptThinRadius(1000.)
+    e.r_0 = au
+    e.rmax = OptThinRadius(10.)
+    e.power = -2.
+    e.dust = dust
+
+    m.set_spherical_polar_grid_auto(2, 2, 2)
+
+    m.set_n_photons(initial=100, imaging=100)
+
+    m.write(random_filename())
