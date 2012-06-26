8f2d775e567d7aff1930f21a88f6b9e55620392f
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-06-26 18:05:24 +0200

    Updated PowerLawEnvelope to new API documentation

---
+++ docs/api/hyperion.densities.PowerLawEnvelope.rst
@@ -0,0 +1,34 @@
+===================================
+hyperion.densities.PowerLawEnvelope
+===================================
+
+.. currentmodule:: hyperion.densities
+
+.. autoclass:: PowerLawEnvelope
+
+   .. rubric:: Attributes
+
+   .. autosummary::
+
+      ~PowerLawEnvelope.mass
+      ~PowerLawEnvelope.rho_0
+      ~PowerLawEnvelope.rmax
+      ~PowerLawEnvelope.rmin
+      ~PowerLawEnvelope.power
+      ~PowerLawEnvelope.dust
+
+   .. rubric:: Methods
+
+   .. autosummary::
+
+      ~PowerLawEnvelope.density
+      ~PowerLawEnvelope.outermost_radius
+      ~PowerLawEnvelope.midplane_cumulative_density
+      ~PowerLawEnvelope.add_bipolar_cavity
+
+   .. rubric:: Methods (detail)
+
+   .. automethod:: PowerLawEnvelope.density
+   .. automethod:: PowerLawEnvelope.outermost_radius
+   .. automethod:: PowerLawEnvelope.midplane_cumulative_density
+   .. automethod:: PowerLawEnvelope.add_bipolar_cavity
--- hyperion/densities/power_law_envelope.py
+++ hyperion/densities/power_law_envelope.py
@@ -13,37 +13,32 @@ from ..util.validator import validate_scalar
 
 
 class PowerLawEnvelope(Envelope):
+    r'''
+    This class implements the density structure for a spherically symmetric
+    power-law envelope, with a density given by:
 
-    def __init__(self, mass=None, rho_0=None, r_0=None, rmin=None, rmax=None, power=None, star=None):
-        '''
-        Initialize a power-law envelope instance. The required parameters are:
+    .. math:: \rho(r) = \rho_0^{\rm env}\,\left(\frac{r}{r_0}\right)^\gamma \\
 
-            mass: envelope mass (g)
-            rmin: inner radius (cm)
-            rmin: output radius (cm)
-            power: power-law exponent
+    One the ``PowerLawEnvelope`` class has been instantiated, the parameters for
+    the density structure can be set via attributes::
 
-        '''
+        >>> from hyperion.util.constants import msun, au, pc
+        >>> envelope = PowerLawEnvelope()
+        >>> envelope.mass = msun
+        >>> envelope.rmin = 0.1 * au
+        >>> envelope.rmax = pc
+    '''
+
+    def __init__(self):
 
         # Basic envelope parameters
-        self.rmin = rmin
-        self.rmax = rmax
-        self.power = power
-        self.r_0 = r_0
+        self.rmin = None
+        self.rmax = None
+        self.power = None
+        self.r_0 = None
 
-        # Envelope Infall
-        if mass is not None and rho_0 is not None:
-            raise Exception("Cannot specify both mass and rho_0")
-
-        if mass is not None:
-            self.mass = mass
-        elif rho_0 is not None:
-            self.rho_0 = rho_0
-        else:
-            self.mass = 0.
-
-        # Central star
-        # self.star = star
+        self.rho_0 = None
+        self.mass = None
 
         # Cavity
         self.cavity = None
@@ -52,6 +47,123 @@ class PowerLawEnvelope(Envelope):
         self.dust = None
 
         self._freeze()
+
+    @property
+    def rmin(self):
+        """inner radius (cm)"""
+        return self._rmin
+
+    @rmin.setter
+    def rmin(self, value):
+        if not isinstance(value, OptThinRadius) and value is not None:
+            validate_scalar('rmin', value, domain='positive', extra=' or an OptThinRadius instance')
+        self._rmin = value
+
+    @property
+    def rmax(self):
+        """outer radius (cm)"""
+        return self._rmax
+
+    @rmax.setter
+    def rmax(self, value):
+        if not isinstance(value, OptThinRadius) and value is not None:
+            validate_scalar('rmax', value, domain='positive', extra=' or an OptThinRadius instance')
+        self._rmax = value
+
+    @property
+    def power(self):
+        '''density power-law exponent'''
+        return self._power
+
+    @power.setter
+    def power(self, value):
+        if value is not None:
+            validate_scalar('power', value, domain='real')
+        self._power = value
+
+    @property
+    def r_0(self):
+        '''radius at which ``rho_0`` is defined (cm)'''
+        return self._r_0
+
+    @r_0.setter
+    def r_0(self, value):
+        if value is not None:
+            validate_scalar('r_0', value, domain='positive')
+        self._r_0 = value
+
+    @property
+    def mass(self):
+        '''total mass (g)'''
+        if self._mass is not None:
+            return self._mass
+        elif self._rho_0 is None:
+            return None
+        else:
+            self._check_all_set()
+            alpha = 3. + self.power
+            mass = self.rho_0 / alpha * \
+                (4. * pi * (self.rmax ** alpha - self.rmin ** alpha) / self.r_0 ** self.power)
+            return mass
+
+    @mass.setter
+    def mass(self, value):
+        if value is not None:
+            validate_scalar('mass', value, domain='positive')
+            if self._rho_0 is not None:
+                logger.warn("Overriding value of rho_0 with value derived from mass")
+                self._rho_0 = None
+        self._mass = value
+
+    @property
+    def rho_0(self):
+        '''density at ``r_0`` (g/cm^3)'''
+        if self._rho_0 is not None:
+            return self._rho_0
+        elif self._mass is None:
+            return None
+        else:
+            self._check_all_set()
+            alpha = 3. + self.power
+            rho_0 = self.mass * alpha / \
+                (4. * pi * (self.rmax ** alpha - self.rmin ** alpha) / self.r_0 ** self.power)
+            return rho_0
+
+    @rho_0.setter
+    def rho_0(self, value):
+        if value is not None:
+            validate_scalar('rho_0', value, domain='positive')
+            if self._mass is not None:
+                logger.warn("Overriding value of mass with value derived from rho_0")
+                self._mass= None
+        self._rho_0 = value
+
+    @property
+    def cavity(self):
+        '''BipolarCavity instance'''
+        return self._cavity
+
+    @cavity.setter
+    def cavity(self, value):
+        if value is None:
+            self._cavity = None
+        else:
+            if not isinstance(value, BipolarCavity):
+                raise ValueError("cavity should be an instance of BipolarCavity")
+            self._cavity = value
+            self._cavity.envelope = self
+
+    @property
+    def dust(self):
+        '''dust properties (filename or dust object)'''
+        return self._dust
+
+    @dust.setter
+    def dust(self, value):
+        if isinstance(value, basestring):
+            self._dust = SphericalDust(value)
+        else:
+            self._dust = value
 
     def _check_all_set(self):
 
@@ -74,12 +186,22 @@ class PowerLawEnvelope(Envelope):
 
     def density(self, grid, ignore_cavity=False):
         '''
-        Find the density of a power-law envelope
+        Return the density grid
 
-        Input is the position of the center of the grid cells in spherical
-        polar coordinates (r, theta, phi), the volume of the grid cells, and a
-        parameter dictionary
+        Parameters
+        ----------
+        grid : SphericalPolarGrid or CylindricalPolarGrid instance
+            The spherical or cylindrical polar grid object containing
+            information about the position of the grid cells.
+
+        Returns
+        -------
+        rho : np.ndarray
+            A 3-dimensional array containing the density of the envelope
+            inside each cell. The shape of this array is the same as
+            ``grid.shape``.
         '''
+
 
         self._check_all_set()
 
@@ -108,13 +230,36 @@ class PowerLawEnvelope(Envelope):
         '''
         Find the outermost radius at which the density of the envelope has
         fallen to `rho`.
+
+        Parameters
+        ----------
+        rho : float
+            The density for which to determine the radius
+
+        Returns
+        -------
+        r : float
+            The radius at which the density has fallen to ``rho``
         '''
         return self.r_0 * (rho / self.rho_0) ** (1. / self.power)
 
     def midplane_cumulative_density(self, r):
         '''
-        Find the cumulative column density as a function of radius from the
-        star in the midplane of a power-law envelope.
+        Find the cumulative column density as a function of radius.
+
+        The cumulative density is measured outwards from the star, and in
+        the midplane.
+
+        Parameters
+        ----------
+        r : np.ndarray
+            Array of values of the radius up to which to tabulate the
+            cumulative density.
+
+        Returns
+        -------
+        rho : np.ndarray
+            Array of values of the cumulative density.
         '''
 
         self._check_all_set()
@@ -126,69 +271,18 @@ class PowerLawEnvelope(Envelope):
         return self.rho_0 * integrate_powerlaw(self.rmin, r.clip(self.rmin, self.rmax), self.power) / self.r_0 ** self.power
 
     def add_bipolar_cavity(self):
+        '''
+        Add a bipolar cavity to the envelope.
+
+        Returns
+        -------
+        cavity : BipolarCavity instance
+            The bipolar cavity instance, which can then be used to set the
+            parameters of the cavity.
+        '''
+
         if self.cavity is not None:
             raise Exception("Envelope already has a bipolar cavity")
         else:
             self.cavity = BipolarCavity()
             return self.cavity
-
-    def __setattr__(self, attribute, value):
-
-        if value is not None:
-
-            # Dust specified as string
-            if attribute == 'dust' and isinstance(value, basestring):
-                Envelope.__setattr__(self, 'dust', SphericalDust(value))
-                return
-
-            # Positive scalars
-            if attribute in ['r_0', 'rho_0', 'mass']:
-                validate_scalar(attribute, value, domain='positive')
-
-            # Scalars
-            if attribute in ['power']:
-                validate_scalar(attribute, value, domain='real')
-
-            # Radii (positive scalars or OptThinRadius instance)
-            if attribute in ['rmin', 'rmax']:
-                if not isinstance(value, OptThinRadius):
-                    validate_scalar(attribute, value, domain='positive', extra=' or an OptThinRadius instance')
-
-            # Bipolar cavity
-            if attribute == 'cavity':
-                if not isinstance(value, BipolarCavity):
-                    raise ValueError("cavity should be an instance of BipolarCavity")
-
-        if attribute == 'mass':
-            if 'rho_0' in self.__dict__:
-                logger.warn("Overriding value of rho_0 with value derived from mass")
-                del self.rho_0
-            object.__setattr__(self, attribute, value)
-        elif attribute == 'rho_0':
-            if 'mass' in self.__dict__:
-                logger.warn("Overriding value of mass with value derived from rho_0")
-                del self.mass
-            object.__setattr__(self, attribute, value)
-        else:
-            Envelope.__setattr__(self, attribute, value)
-
-        if attribute == 'cavity' and isinstance(value, BipolarCavity):
-            self.cavity.envelope = self
-
-    def __getattr__(self, attribute):
-
-        if attribute == 'rho_0':
-            self._check_all_set()
-            alpha = 3. + self.power
-            rho_0 = self.mass * alpha / \
-                (4. * pi * (self.rmax ** alpha - self.rmin ** alpha) / self.r_0 ** self.power)
-            return rho_0
-
-        if attribute == 'mass':
-            self._check_all_set()
-            alpha = 3. + self.power
-            mass = self.rho_0 / alpha * \
-                (4. * pi * (self.rmax ** alpha - self.rmin ** alpha) / self.r_0 ** self.power)
-            return mass
-
-        raise AttributeError(attribute)
