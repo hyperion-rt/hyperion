e61917e18efa3340df1c68ff057732a8a9f77d2b
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-07-21 16:42:06 +0200

    Remove unused code from hyperion/__init__.py

--- hyperion/__init__.py
+++ hyperion/__init__.py
@@ -4,27 +4,7 @@ import os
 import glob
 import hashlib
 
-import h5py
-
 from .version import __version__
-
-data_dir = __path__[0] + '/data/'
-
-datafiles = {}
-for datafile in glob.glob(os.path.join(data_dir, '*.hdf5')):
-    f = h5py.File(datafile)
-    hash = f.attrs['asciimd5'].decode('utf-8')
-    datafiles[hash] = os.path.abspath(datafile)
-    f.close()
-
-def get_HDF5_datafile(filename):
-
-    h = hashlib.md5(file(filename,'rb').read()).hexdigest()
-
-    if h in datafiles:
-        return datafiles[h]
-    else:
-        raise Exception("File does not exist")
 
 # Set up the test function
 _test_runner = None
