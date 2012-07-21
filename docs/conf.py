70ec371d2a5e0acbed58fd5ca963b062691878fd
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-07-21 16:49:28 +0200

    Make sure that h5py and ATpy are mocked on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -198,3 +198,25 @@ latex_documents = [
 autosummary_generate = True
 numpydoc_show_class_members = False
 autoclass_content = 'class'
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+
+    class Mock(object):
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __call__(self, *args, **kwargs):
+            return Mock()
+
+        @classmethod
+        def __getattr__(self, name):
+            if name in ('__file__', '__path__'):
+                return '/dev/null'
+            elif name[0] == name[0].upper():
+                return type(name, (), {})
+            else:
+                return Mock()
+
+    MOCK_MODULES = ['h5py', 'atpy']
+    for mod_name in MOCK_MODULES:
+        sys.modules[mod_name] = Mock()
