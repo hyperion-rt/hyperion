2faa9cfd963929edd293d387f781de1ca24d8bdd
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-09-04 15:27:31 +0200

    Fixed bug that prevented BipolarCavity from being used

--- hyperion/model/tests/test_analytical_yso.py
+++ hyperion/model/tests/test_analytical_yso.py
@@ -292,3 +292,62 @@ def test_rmin_zero(grid_type):
         with pytest.raises(ValueError) as exc:
             m.set_spherical_polar_grid_auto(100, 20, 3)
         assert exc.value.args[0] == "R_min is 0, so cannot set up the grid cell walls automatically. Use set_spherical_polar_grid() instead to specify the cell wall positions."
+
+
+def test_complete():
+
+    m = AnalyticalYSOModel()
+
+    m.star.radius = 1.
+    m.star.temperature = 1000.
+    m.star.luminosity = 1.
+
+    d = m.add_flared_disk()
+    d.rmin = 0.1
+    d.rmax = 10.
+    d.r_0 = 10.
+    d.h_0 = 1.
+    d.p = -1
+    d.beta = 1.25
+    d.dust = get_test_dust()
+
+    e = m.add_ulrich_envelope()
+    e.rmin = 0.1
+    e.rmax = 10.
+    e.rho_0 = 1.
+    e.rc = 2.
+    e.dust = get_test_dust()
+
+    c1 = e.add_bipolar_cavity()
+    c1.dust = get_test_dust()
+    c1.theta_0 = 10.
+    c1.power = 1.2
+    c1.r_0 = 3.
+    c1.rho_0 = 2.3
+
+    p = m.add_power_law_envelope()
+    p.rmin = 0.2
+    p.rmax = 11.
+    p.mass = 10.
+    p.power = -1.
+    p.r_0 = 2.
+    p.dust = get_test_dust()
+
+    c2 = p.add_bipolar_cavity()
+    c2.dust = get_test_dust()
+    c2.theta_0 = 20
+    c2.power = 1.3
+    c2.r_0 = 4.
+    c2.rho_0 = 9.9
+
+    a = m.add_ambient_medium()
+    a.rmin = 0.1
+    a.rmax = 10.
+    a.rho = 1.
+    a.dust = get_test_dust()
+
+    m.set_spherical_polar_grid_auto(399, 199, 1)
+
+    m.set_n_photons(initial=0, imaging=0)
+
+    m.write(random_filename())
