7a309ea59918b64f9fe7dde6157c8ea350d37796
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-14 23:22:10 +0200

    Fully test OutputConf

--- hyperion/conf/tests/test_conf_io.py
+++ hyperion/conf/tests/test_conf_io.py
@@ -4,6 +4,7 @@ from __future__ import print_function, division
 
 import string
 import random
+from itertools import product
 
 import h5py
 import numpy as np
@@ -21,19 +22,17 @@ def virtual_file():
     return h5py.File(random_id(), driver='core', backing_store=False)
 
 
-def test_io_output_conf():
+@pytest.mark.parametrize(('attribute', 'value'),
+                         list(product(['output_density', 'output_density_diff',
+                         'output_specific_energy', 'output_n_photons'],
+                         ['none', 'last', 'all'])))
+def test_io_output_conf(attribute, value):
     o1 = OutputConf()
-    o1.output_density = 'last'
-    o1.output_density_diff = 'none'
-    o1.output_specific_energy = 'all'
-    o1.output_n_photons = 'last'
+    setattr(o1, attribute, value)
     v = virtual_file()
     o1.write(v)
     o2 = OutputConf.read(v)
-    assert o2.output_density == o1.output_density
-    assert o2.output_density_diff == o1.output_density_diff
-    assert o2.output_specific_energy == o1.output_specific_energy
-    assert o2.output_n_photons == o1.output_n_photons
+    assert getattr(o2, attribute) == value
 
 
 # TODO: implement tests for RunConf and ImageConf (and subclasses)
