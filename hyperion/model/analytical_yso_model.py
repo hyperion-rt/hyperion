e83a7f4e9f301acc43f4aff2e7e850a16af3bb0c
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-16 19:05:05 +0200

    Initial (untested) version of HSEQ function

--- hyperion/model/analytical_yso_model.py
+++ hyperion/model/analytical_yso_model.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 
 from copy import deepcopy
+import multiprocessing
 
 import numpy as np
 
@@ -756,7 +757,7 @@ class AnalyticalYSOModel(Model):
                 if not disk.dust:
                     raise Exception("Disk %i dust not set" % (i + 1))
                 Model.add_density_grid(self, disk.density(self.grid), disk.dust,
-                                      merge_if_possible=merge_if_possible)
+                                       merge_if_possible=merge_if_possible)
 
         for i, envelope in enumerate(self.envelopes):
 
@@ -847,3 +848,146 @@ class AnalyticalYSOModel(Model):
                     copy=copy, absolute_paths=absolute_paths,
                     wall_dtype=wall_dtype, physics_dtype=physics_dtype,
                     overwrite=overwrite)
+
+
+# TODO - HSEQ only works if density is gas density - fix that
+
+def hseq_profile(w, z, temperature, mstar):
+    """
+    Compute the new (normalized) density profile
+    corresponding to a given temperature profile
+
+    Parameters
+    ----------
+    w : float
+        The cylindrical radius at which to compute the profile (in cm)
+    z : np.ndarray
+        The z coordinates of the cells at radius w (in cm)
+    temperature : np.ndarray
+        The temperatures in the cells (in K)
+    mstar : float
+        The mass of the star (in g)
+    """
+
+    from hyperion.util.constants import G, m_h, k
+    from ..util.integrate import integrate, integrate_subset
+
+    # Compute the integrand
+    integrand = z / temperature / (w**2 + z**2)**1.5
+
+    # Compute the integral for all cells
+    i = np.array([integrate_subset(z, integrand, 0., zmax) for zmax in z])
+    i[z < 0] = -i[z < 0]
+
+    # Compute the factor for the integrand
+    mu = 2.33  # TODO: set as parameter?
+    factor = G * mstar * mu * m_h / k
+
+    # Compute the profile
+    density = np.exp(-i * factor) / temperature
+
+    # Normalize the density profile
+    density = density / integrate(z, density)
+
+    return density
+
+
+def run_with_vertical_hseq(prefix, model, n_iter=10, mpi=False, n_processes=multiprocessing.cpu_count(), overwrite=False):
+    """
+    Run a model with vertical hydrostatic equilibrium.
+
+    The hydrostatic equilibrium condition is only applied to the disk
+    components. The following requirements must be met:
+
+    - The model should be an AnalyticalYSOModel
+    - The model should be defined on a cylindrical polar grid
+    - The stellar mass should be set
+    - The model should include at least one disk
+
+    Parameters
+    ----------
+    prefix : str
+        The prefix for the output
+    model : `~hyperion.model.analytical_yso_model.AnalyticalYSOModel`
+        The model to run
+    n_iter : int, optional
+        The number of iterations to run the model for
+    mpi : bool, optional
+        Whether to run the model in parallel
+    n_processes : int, optional
+        The number of processes to use if ``mpi`` is ``True``
+    overwrite : bool, optional
+        Whether to overwrite previous files
+    """
+
+    from ..grid import CylindricalPolarGrid
+    from .model_output import ModelOutput
+    from ..util.integrate import integrate
+
+    if not isinstance(model, AnalyticalYSOModel):
+        raise TypeError("Can only run hydrostatic equilibrium for AnalyticalYSOModel instances")
+
+    if not isinstance(model, CylindricalPolarGrid):
+        raise TypeError("Can only run hydrostatic equilibrium for models with cylindrical polar grids")
+
+    if model.star.mass is None:
+        raise ValueError("Stellar mass needs to be defined for calculation of hydrostatic equilibrium")
+
+    if len(model.disks) == 0:
+        raise ValueError("Can only run hydrostatic equilibrium for models with disks")
+    else:
+        n_disks = len(model.disks)
+
+    # Write out initial model
+    model.write(prefix + '_initial.rtin', overwrite=overwrite)
+
+    # Run the initial model
+    mo = model.run(prefix + '_initial.rtout', overwrite=overwrite,
+                   mpi=mpi, n_processes=n_processes)
+
+    previous = prefix + '_initial.rtout'
+
+    for iteration in range(n_iter):
+
+        # Read in output
+        mo = ModelOutput(previous)
+
+        # Extract the quantities
+        g = mo.get_quantities()
+
+        # Get wall positions
+        rw, zw = g.w_wall, g.z_wall
+
+        # Make a 2-d grid of wall positions
+        R, Z = np.meshgrid(rw, zw)
+
+        # Extract density and temperature
+        density = g['density']
+        temperature = g['temperature']
+
+        # TODO: need to find a better way than just assuming the first n
+        # density grids are disks
+
+        for idisk in range(n_disks):
+
+            # Vertically extrapolate temperatures
+            for i in range(len(g.w)):
+                for j in range(len(g.phi)):
+                    reset = temperature[idisk][j, :, i] < 1.
+                    temperature[idisk][j, reset, i] = np.max(temperature[idisk][j, :, i])  # shouldn't be max, but will do for now
+
+            # Compute new density
+            for i in range(len(g.w)):
+                for j in range(len(g.phi)):
+                    density[idisk][j, :, i] = hseq_profile(g.w[i], g.z, temperature[idisk][j, :, i], model.star.mass) * integrate(g.z, density[idisk][j, :, i])
+
+        # Instantiate new model based on previous
+        m = Model.read(previous)
+
+        # Override the density
+        m.grid['density'] = density
+
+        # Write and run
+        m.write('{0:s}_{1:05d}.rtin'.format(prefix, iteration), overwrite=overwrite)
+        m.run('{0:s}_{1:05d}.rtout'.format(prefix, iteration),
+              overwrite=overwrite, mpi=mpi, n_processes=n_processes)
