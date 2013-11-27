cdd92d77480b34b2fa7a825b79d2617d7f6e23fa
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-11-27 12:02:49 +0100

    Added test for SPH gridding

---
+++ hyperion/importers/tests/test_sph.py
@@ -0,0 +1,35 @@
+import h5py
+import numpy as np
+
+from ..sph import construct_octree
+
+
+def test_construct_octree():
+
+    np.random.seed(0)
+
+    N = 5000
+    px = np.random.uniform(-10., 10., N)
+    py = np.random.uniform(-10., 10., N)
+    pz = np.random.uniform(-10., 10., N)
+    mass = np.random.uniform(0., 1., N)
+    sigma = np.random.uniform(0., 0.1, N)
+
+    o = construct_octree(0.1, 0.2, 0.3, 6., 5., 4.,
+                         px, py, pz,
+                         sigma, mass, n_levels=10)
+
+    # The following lines can be used to write out the reference file if the
+    # SPH gridding code is updated.
+    # f = h5py.File('reference_octree.hdf5', 'w')
+    # o.write(f)
+    # f.close()
+
+    from hyperion.grid import OctreeGrid
+    f = h5py.File('reference_octree.hdf5', 'r')
+    o_ref = OctreeGrid()
+    o_ref.read(f)
+    f.close()
+
+    assert np.all(o_ref.refined == o.refined)
+    assert np.all(o_ref['density'][0].array == o['density'][0].array)
