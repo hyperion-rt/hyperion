8852a7d47575e0092fa44a354c4f962a1c7db047
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-05-04 17:01:35 +0200

    Docstrings

--- hyperion/importers/sph.py
+++ hyperion/importers/sph.py
@@ -43,7 +43,12 @@ def construct_octree(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
                      stopping_criterion=DEFAULT_STOPPING_CRITERION,
                      mode='exact'):
     """
-    Construct an Octree grid from SPH particles
+    Construct an Octree grid from SPH particles.
+
+    This will set up the octree, and then compute the densities. It is
+    equivalent to calling
+    :func:`~hyperion.importers.sph.compute_octree_geometry` and
+    func:`~hyperion.importers.sph.compute_octree_density`.
 
     Parameters
     ----------
@@ -71,24 +76,54 @@ def construct_octree(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
     Returns
     -------
     grid : `~hyperion.grid.octree_grid.OctreeGrid`
-        The octree grid
+        The octree grid with the densities set
     """
 
     # Set up geometrical OctreeGrid
-    octree = _setup_octree(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
-                           n_levels=n_levels, stopping_criterion=stopping_criterion)
+    octree = compute_octree_geometry(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
+                                     n_levels=n_levels, stopping_criterion=stopping_criterion)
 
     # Compute densities and add to octree
-    density = _compute_octree_densities(octree, px, py, pz, sigma, mass, mode=mode)
+    density = compute_octree_densities(octree, px, py, pz, sigma, mass, mode=mode)
     octree['density'] = []
     octree['density'].append(density)
 
     return octree
 
 
-def _setup_octree(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
-                  n_levels=None,
-                  stopping_criterion=DEFAULT_STOPPING_CRITERION):
+def compute_octree_geometry(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
+                            n_levels=None,
+                            stopping_criterion=DEFAULT_STOPPING_CRITERION):
+    """
+    Generate an Octree grid from SPH particles.
+
+    Note that this does not set the densities.
+
+    Parameters
+    ----------
+    x, y, z : float
+        The center of the top-level cell
+    dx, dy, dz : float
+        The half-width of the top-level cell
+    px, py, pz : np.ndarray
+        The positions of the SPH particles
+    sigma : np.ndarray
+        The sigma of the SPH particles (assuming a Gaussian Kernel)
+    mass : np.ndarray
+        The mass of the SPH particles
+    n_levels : int, optional
+        Maximum number of levels in the octree. If not specified, then the
+        octree will keep dividing until all cells contain at most one particle.
+    stopping_criterion : func, optional
+        A function that is used to determine whether to stop refining a cell.
+        If not set, then refinement stops once there are two or fewer particles
+        in a cell.
+
+    Returns
+    -------
+    grid : `~hyperion.grid.octree_grid.OctreeGrid`
+        The octree grid
+    """
 
     if n_levels is None:
         n_levels = np.inf
@@ -104,7 +139,27 @@ def _setup_octree(x, y, z, dx, dy, dz, px, py, pz, sigma, mass,
     return octree
 
 
-def _compute_octree_densities(octree, px, py, pz, sigma, mass, mode='exact'):
+def compute_octree_densities(octree, px, py, pz, sigma, mass, mode='exact'):
+    """
+    Compute the density of the cells in an Octree.
+
+    Parameters
+    ----------
+    px, py, pz : np.ndarray
+        The positions of the SPH particles
+    sigma : np.ndarray
+        The sigma of the SPH particles (assuming a Gaussian Kernel)
+    mass : np.ndarray
+        The mass of the SPH particles
+    mode : { 'fast', 'exact' }
+        Whether to properly compute the integral over the kernel ('exact') or
+        simply count the particles in each cell ('fast')
+
+    Returns
+    -------
+    density : `~numpy.ndarray`
+        The density in each cell.
+    """
 
     # Find limits for each cell
     xmin, xmax, ymin, ymax, zmin, zmax = octree.limits
