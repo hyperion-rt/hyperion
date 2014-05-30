128dcb5067aa54dc3078ab279306aba98a1ce4c0
Francesco Biscani <bluescarni@gmail.com>
2014-05-30 11:08:03 +0200

    Rewrite more functions in C and parallelize the computation of volumes and bbs.

--- hyperion/grid/_voronoi_core.c
+++ hyperion/grid/_voronoi_core.c
@@ -7,13 +7,19 @@
 /* Define docstrings */
 static char module_docstring[] = "C implementation of utility functions used in Voronoi grids";
 static char simplex3d_volume_docstring[] = "Compute the volume of a 3d simplex";
+static char neighbours_list_loop_docstring[] = "Inner loop for the computation of the neighbours list";
+static char region_in_domain_docstring[] = "Test if region is in domain";
 
 /* Declare the C functions here. */
 static PyObject *_simplex3d_volume(PyObject *self, PyObject *args);
+static PyObject *_neighbours_list_loop(PyObject *self, PyObject *args);
+static PyObject *_region_in_domain(PyObject *self, PyObject *args);
 
 /* Define the methods that will be available on the module. */
 static PyMethodDef module_methods[] = {
     {"_simplex3d_volume", _simplex3d_volume, METH_VARARGS, simplex3d_volume_docstring},
+    {"_neighbours_list_loop", _neighbours_list_loop, METH_VARARGS, neighbours_list_loop_docstring},
+    {"_region_in_domain", _region_in_domain, METH_VARARGS, region_in_domain_docstring},
     {NULL, NULL, 0, NULL}
 };
 
@@ -68,7 +74,7 @@ static PyObject *_simplex3d_volume(PyObject *self, PyObject *args)
 
     if (n != 4 || m != 3) {
         PyErr_SetString(PyExc_RuntimeError, "Invalid array dimension(s).");
-        Py_DECREF(array);
+        Py_XDECREF(array);
         return NULL;
     }
 
@@ -103,7 +109,7 @@ static PyObject *_simplex3d_volume(PyObject *self, PyObject *args)
     double retval = fabs(a*(e*i-f*h)-b*(d*i-f*g)+c*(d*h-e*g)) / 6.;
 
     /* Clean up. */
-    Py_DECREF(array);
+    Py_XDECREF(array);
 
     /* Build the output value. */
     PyObject *ret = Py_BuildValue("d", retval);
@@ -113,4 +119,97 @@ static PyObject *_simplex3d_volume(PyObject *self, PyObject *args)
     }
 
     return ret;
+}
+
+static PyObject *_neighbours_list_loop(PyObject *self, PyObject *args)
+{
+    PyObject *simplices_obj, *nl_obj;
+
+    if (!PyArg_ParseTuple(args, "OO", &simplices_obj, &nl_obj))
+        return NULL;
+
+    /* Interpret the input objects as `numpy` arrays. */
+    PyObject *s_array = PyArray_FROM_OTF(simplices_obj, NPY_INT32, NPY_IN_ARRAY);
+
+    /* Handle invalid input. */
+    if (s_array == NULL || !PyList_Check(nl_obj)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid input objects.");
+        Py_XDECREF(s_array);
+        return NULL;
+    }
+
+    /* Get the array dimensions. */
+    int n = (int)PyArray_DIM(s_array, 0), m = (int)PyArray_DIM(s_array, 1);
+
+    /* Get pointer to the data. */
+    int32_t *data = (int32_t *)PyArray_DATA(s_array);
+
+    // Run the loop.
+    int i, j1, j2;
+    for (i = 0; i < n; ++i) {
+        int32_t *simplex = data + i * m;
+        for (j1 = 0; j1 < m; ++j1) {
+            int32_t s_idx = simplex[j1];
+            for (j2 = 0; j2 < m; ++j2) {
+                int32_t n_idx = simplex[j2];
+                if (n_idx != s_idx) {
+                    // This one is a borrowed reference, no need to mess
+                    // with refcount.
+                    PyObject *tmp_set = PyList_GetItem(nl_obj,s_idx);
+                    // This is a new reference.
+                    PyObject *n_obj = PyLong_FromLong(n_idx);
+                    PySet_Add(tmp_set,n_obj);
+                    Py_XDECREF(n_obj);
+                }
+            }
+        }
+    }
+
+    // Final cleanup.
+    Py_XDECREF(s_array);
+
+    Py_RETURN_NONE;
+}
+
+static PyObject *_region_in_domain(PyObject *self, PyObject *args)
+{
+    PyObject *region_obj, *vertices_obj, *domain_obj;
+
+    if (!PyArg_ParseTuple(args, "OOO", &region_obj, &vertices_obj, &domain_obj))
+        return NULL;
+
+    /* Interpret the input objects as `numpy` arrays. */
+    PyObject *v_array = PyArray_FROM_OTF(vertices_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject *d_array = PyArray_FROM_OTF(domain_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+
+    /* Handle invalid input. */
+    if (v_array == NULL || d_array == NULL || !PyList_Check(region_obj)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid input objects.");
+        Py_XDECREF(v_array);
+        Py_XDECREF(d_array);
+        return NULL;
+    }
+
+    double *v_data = (double*)PyArray_DATA(v_array);
+    double *d_data = (double*)PyArray_DATA(d_array);
+
+    int ndim = (int)PyArray_DIM(v_array, 1);
+
+    int r_size = PyList_Size(region_obj), i, j;
+
+    for (i = 0; i < r_size; ++i) {
+        int vertex_idx = PyLong_AsLong(PyList_GetItem(region_obj,i));
+        double *vertex_ptr = v_data + vertex_idx * ndim;
+        for (j = 0; j < ndim; ++j) {
+            if (vertex_ptr[j] < d_data[2*j] || vertex_ptr[j] > d_data[2*j + 1]) {
+                Py_XDECREF(v_array);
+                Py_XDECREF(d_array);
+                Py_RETURN_FALSE;
+            }
+        }
+    }
+
+    Py_XDECREF(v_array);
+    Py_XDECREF(d_array);
+    Py_RETURN_TRUE;
 }
--- hyperion/grid/voronoi_helpers.py
+++ hyperion/grid/voronoi_helpers.py
@@ -1,19 +1,103 @@
+import multiprocessing as _mp
+
+# Function used to disable ctrl+c in sub processes.
 def _do_nothing(signum, frame):
     pass
 
+# Parallel functor for the computation of the initial
+# tessellations.
 def _par_tess(t):
     import gc, signal
+
     signal.signal(signal.SIGINT,_do_nothing)
+
     retval = t[0](t[1])
-    print(gc.collect())
+
     return retval
 
+# Parallel functor for the computation of the volumes
+# and the bounding boxes.
+def _par_vol_bb(t):
+    import gc, signal, numpy as np
+    from scipy.spatial import Delaunay
+
+    signal.signal(signal.SIGINT,_do_nothing)
+
+    # Unpack the arguments from the input tuple.
+    chunk,points,point_region,protruding_cells,regions,v_vertices,domain = t
+    start, end = chunk
+
+    ndim = len(domain)
+
+    # Function to convert from site indices to region indices.
+    sidx_to_ridx = lambda sidx: point_region[sidx]
+
+    # Init the array of bounding boxes with zeroes.
+    bb_arr = np.zeros([end - start,2 * ndim])
+
+    # Inite the volmes array with -1s.
+    vol_arr = np.empty([end - start],dtype=np.dtype(float))
+    vol_arr.fill(-1.)
+
+    # Main loop for the computation of the volumes and bounding boxes.
+    for i in range(start,end):
+        ridx = sidx_to_ridx(i)
+
+        # Protruding cells will have a volume of -1 and bounding box
+        # of [0 ... 0].
+        if not ridx in protruding_cells:
+            cell = regions[ridx]
+            vertices = v_vertices[cell]
+
+            # Decompose the Voronoi region into simplices.
+            dtess = Delaunay(vertices)
+
+            # Accumulate the volumes of the simplices.
+            volume = 0.
+            for simplex in dtess.simplices:
+                volume += _simplex_volume(dtess.points[simplex])
+            vol_arr[i - start] = volume
+
+            # Bounding box.
+            # Initialise min/max with the coordinates of the first vertex.
+            bb_arr[i - start][0:ndim] = vertices[0]
+            bb_arr[i - start][ndim:] = vertices[0]
+
+            # Iterate on the remaining vertices and update the minmax values as needed.
+            for vertex in vertices[1:]:
+                for j in range(0,len(vertex)):
+                    if vertex[j] < bb_arr[i - start][j]:
+                        bb_arr[i - start][j] = vertex[j]
+                    if vertex[j] > bb_arr[i - start][ndim + j]:
+                        bb_arr[i - start][ndim + j] = vertex[j]
+
+    return vol_arr, bb_arr
+
+# Compute the volume of a simplex.
+# http://en.wikipedia.org/wiki/Simplex#Volume
+def _simplex_volume(simplex):
+    n = len(simplex) - 1
+
+    # Special casing for 3d, use the C version.
+    if n == 3:
+        from ._voronoi_core import _simplex3d_volume
+        return _simplex3d_volume(simplex)
+
+    from math import gamma
+    import numpy as np
+
+    matrix = np.empty((n,n))
+    for i in range(0,n):
+        matrix[i] = simplex[i + 1] - simplex[0]
+
+    det = np.linalg.det(matrix)
+    return abs(det / gamma(n + 1))
+
 class voronoi_grid(object):
-    def __init__(self,sites,domain):
+    def __init__(self,sites,domain,ncpus = _mp.cpu_count()):
         from scipy.spatial import Delaunay, Voronoi
         import numpy as np
         from copy import deepcopy
-        import multiprocessing as mp
 
         # Validate input.
         if not isinstance(sites,np.ndarray) or sites.dtype.kind != 'f':
@@ -41,9 +125,8 @@ class voronoi_grid(object):
                     raise ValueError('a site is outside the domain')
 
         # Setup the process pool.
-        ncpus = mp.cpu_count()
         print('Initing pool with {0} cpus.'.format(ncpus))
-        self._pool = mp.Pool(ncpus)
+        self._pool = _mp.Pool(ncpus)
         self._ncpus = ncpus
 
         # Store the domain.
@@ -59,6 +142,10 @@ class voronoi_grid(object):
         # user-facing should be presented in that convention.
         self._del_tess, self._vor_tess = self._pool.map(_par_tess,[(Delaunay,sites),(Voronoi,sites)])
 
+        # Close the pool for now, free resources.
+        self._pool.close()
+        self._pool.join()
+
         # Build the dict for the mapping from region idx to site idx.
         self._r_to_s_dict = dict([(t[1],t[0]) for t in enumerate(self._vor_tess.point_region)])
 
@@ -67,28 +154,21 @@ class voronoi_grid(object):
 
         # Identify the cells that are protruding from the box (including infinite cells).
         # Here we are using the Voronoi regions, so everything is indexed according to the regions.
-        protruding_cells = set()
-        regions_copy = deepcopy(self._vor_tess.regions)
-        for i in range(len(self._vor_tess.regions)):
-            region = self._vor_tess.regions[i]
-            if -1 in region or not self._region_in_domain(region):
-                regions_copy[i] = []
-                protruding_cells.add(i)
-        self._protruding_cells = protruding_cells
+        self._protruding_cells = self._compute_protruding_cells()
 
+        # Compute and store the table.
+        self._neighbours_table = self._compute_neighbours_table()
+
+        # NOTE: this is used for the debug plotting methods, disabled for now.
         # The wall cells will be all the neighbours of the protruding ones, excluding the
         # protruding ones themselves.
-        wall_cells = set()
-        for cell in protruding_cells:
-            for neighbour in self._nl[self._ridx_to_sidx(cell)]:
-                if neighbour != -1 and not self._sidx_to_ridx(neighbour) in protruding_cells:
-                    wall_cells.add(self._sidx_to_ridx(neighbour))
-        self._new_regions = regions_copy
-        self._wall_cells = wall_cells
-
-        # Close the pool.
-        self._pool.close()
-        self._pool.join()
+        #wall_cells = set()
+        #for cell in protruding_cells:
+            #for neighbour in self._nl[self._ridx_to_sidx(cell)]:
+                #if neighbour != -1 and not self._sidx_to_ridx(neighbour) in protruding_cells:
+                    #wall_cells.add(self._sidx_to_ridx(neighbour))
+        #self._new_regions = regions_copy
+        #self._wall_cells = wall_cells
 
     # Convert site idx (i.e., indexing with respect to the input sites, used in the Delaunay tessellation
     # and in the neighbours list) to the indexing of Voronoi regions as resulting from the Voronoi tessellation.
@@ -99,23 +179,19 @@ class voronoi_grid(object):
     def _ridx_to_sidx(self,ridx):
         return self._r_to_s_dict[ridx]
 
-    # Compute neighbours list.
-    def _compute_neighbours(self):
-        import numpy as np
-        neigh_list = [set() for i in range(0,len(self._del_tess.points))]
-        for simplex in self._del_tess.simplices:
-            for site_idx in simplex:
-                for neigh_idx in simplex:
-                    if neigh_idx != site_idx:
-                        neigh_list[site_idx].add(neigh_idx)
-        # Transform the list into a NumPy array.
-        n, m = len(neigh_list), len(max(neigh_list,key = lambda l: len(l)))
-        # Fill with -1 values.
-        retval = np.empty([n,m],dtype=np.int32)
-        retval.fill(-1)
-        for i in range(n):
-            retval[i][:len(neigh_list[i])] = list(neigh_list[i])
-        return retval
+    # Computation of protruding cells.
+    def _compute_protruding_cells(self):
+        #from copy import deepcopy
+        from ._voronoi_core import _region_in_domain
+        protruding_cells = set()
+        #regions_copy = deepcopy(self._vor_tess.regions)
+        for i in range(len(self._vor_tess.regions)):
+            region = self._vor_tess.regions[i]
+            #if -1 in region or not self._region_in_domain(region):
+            if -1 in region or not _region_in_domain(region,self._vor_tess.vertices,self._domain):
+                #regions_copy[i] = []
+                protruding_cells.add(i)
+        return protruding_cells
 
     # Test if a Voronoi region is entirely within the domain.
     def _region_in_domain(self,region):
@@ -125,6 +201,25 @@ class voronoi_grid(object):
                 if coord < limit[0] or coord > limit[1]:
                     return False
         return True
+
+    # Compute neighbours list.
+    def _compute_neighbours(self):
+        import numpy as np
+        from ._voronoi_core import _neighbours_list_loop
+        neigh_list = [set() for i in range(0,len(self._del_tess.points))]
+
+        # Run the main loop from C.
+        _neighbours_list_loop(self._del_tess.simplices,neigh_list)
+
+        # Transform the list into a NumPy array.
+        n, m = len(neigh_list), len(max(neigh_list,key = lambda l: len(l)))
+        # Fill with -1 values.
+        retval = np.empty([n,m],dtype=np.int32)
+        retval.fill(-1)
+        for i in range(n):
+            retval[i][:len(neigh_list[i])] = list(neigh_list[i])
+
+        return retval
 
     # A couple of plotting methods used for debug.
     def _plot_original(self):
@@ -166,100 +261,49 @@ class voronoi_grid(object):
         for i in range(len(self._new_regions)):
             plot_region(i)
 
-    # Compute the volume of a simplex.
-    # http://en.wikipedia.org/wiki/Simplex#Volume
-    def _simplex_volume(self,simplex):
-        n = len(simplex) - 1
-
-        # Special casing for 3d, use the C version.
-        if n == 3:
-            from ._voronoi_core import _simplex3d_volume
-            return _simplex3d_volume(simplex)
-
-        from math import gamma
-        import numpy as np
-
-        # Cache the matrix used for volume computation.
-        if not hasattr(self,'_zero_msimplex'):
-            matrix = np.zeros((n,n))
-            self._zero_msimplex = matrix
-        else:
-            matrix = self._zero_msimplex
-
-        for i in range(0,n):
-            matrix[i] = simplex[i + 1] - simplex[0]
-
-        det = np.linalg.det(matrix)
-        return abs(det / gamma(n + 1))
-
-    def _compute_bb(self):
-        import numpy as np
-
-        ndim = len(self._domain)
-        bb_arr = np.zeros([len(self._vor_tess.points),2 * ndim])
-        for i in range(len(self._vor_tess.points)):
-            ridx = self._sidx_to_ridx(i)
-
-            # Do something only if the cell is not protruding. If it protrudes,
-            # the output array has already been inited with zeroes.
-            if not ridx in self._protruding_cells:
-                cell = self._vor_tess.regions[ridx]
-                vertices = self._vor_tess.vertices[cell]
-
-                # Initialise min/max with the coordinates of the first vertex.
-                bb_arr[i][0:ndim] = vertices[0]
-                bb_arr[i][ndim:] = vertices[0]
-
-                # Iterate on the remaining vertices and update the minmax values as needed.
-                for vertex in vertices[1:]:
-                    for j in range(0,len(vertex)):
-                        if vertex[j] < bb_arr[i][j]:
-                            bb_arr[i][j] = vertex[j]
-                        if vertex[j] > bb_arr[i][ndim + j]:
-                            bb_arr[i][ndim + j] = vertex[j]
-
-        return bb_arr
-
     # Compute and return the neighbours table.
-    @property
-    def neighbours_table(self):
+    def _compute_neighbours_table(self):
         from astropy.table import Table
         import numpy as np
         from scipy.spatial import Delaunay
-        from copy import deepcopy
+
+        # Revive the pool.
+        print('Initing pool with {0} cpus.'.format(self._ncpus))
+        self._pool = _mp.Pool(self._ncpus)
 
         ndim = len(self._domain)
 
-        # Fetch the cached value, if it exists.
-        #if hasattr(self,'__neighbours_table'):
-            #return deepcopy(self.__neighbours_table)
+        ntot = len(self._del_tess.points)
+        chunk_size = ntot // self._ncpus
+        chunks = []
+        for i in range(0,self._ncpus - 1):
+            chunks.append((i * chunk_size,(i + 1) * chunk_size))
+        # Last chunk.
+        chunks.append(((self._ncpus - 1) * chunk_size,ntot))
 
-        # Compute the volumes.
-        vol_arr = np.empty([len(self._vor_tess.points)],dtype=np.dtype(float))
-        vol_arr.fill(-1.)
+        vblists = self._pool.map(_par_vol_bb,[(chunk,self._vor_tess.points,self._vor_tess.point_region,
+                                self._protruding_cells, self._vor_tess.regions, self._vor_tess.vertices, self._domain) for chunk in chunks])
 
-        for i in range(len(self._vor_tess.points)):
-            # Protruding cells will have a volume of -1.
-            if not self._sidx_to_ridx(i) in self._protruding_cells:
-                cell = self._vor_tess.regions[self._sidx_to_ridx(i)]
-                vertices = self._vor_tess.vertices[cell]
+        self._pool.close()
+        self._pool.join()
 
-                # Decompose the Voronoi region into simplices.
-                dtess = Delaunay(vertices)
+        vlist = [t[0] for t in vblists]
+        blist = [t[1] for t in vblists]
 
-                # Accumulate the volumes of the simplices.
-                volume = 0.
-                for simplex in dtess.simplices:
-                    volume += self._simplex_volume(dtess.points[simplex])
-                vol_arr[i] =volume
+        vol_arr = np.hstack(tuple(vlist))
 
         # Bounding boxes.
-        bb_arr = self._compute_bb()
+        bb_arr = np.vstack(tuple(blist))
 
         # Build the table, including the sites coordinates.
         t = Table([self._vor_tess.points,self._nl,vol_arr,bb_arr[:,0:ndim],bb_arr[:,ndim:]],names=('coordinates','neighbours','volume','bb_min','bb_max'))
 
-        # Store the table for later use.
-        self._neighbours_table = t
+        self._pool.close()
+        self._pool.join()
 
+        return t
+
+    @property
+    def neighbours_table(self):
+        from copy import deepcopy
         return deepcopy(self._neighbours_table)
