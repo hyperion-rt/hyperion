885aade96a2e9d5d6c0c6e3a9aac32ce0a1c3fbc
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-29 15:45:53 +0100

    Added unit tests for VelocityMixin

--- hyperion/sources/tests/test_mixins.py
+++ hyperion/sources/tests/test_mixins.py
@@ -3,7 +3,7 @@ from __future__ import print_function, division
 import numpy as np
 from astropy.tests.helper import pytest
 
-from ..mixins import PositionMixin
+from ..mixins import PositionMixin, VelocityMixin
 from ..source import Source
 
 class TestPositionMixin(object):
@@ -50,3 +50,50 @@ class TestPositionMixin(object):
         with pytest.raises(ValueError) as exc:
             self.p.position = position
         assert exc.value.args[0] == "position should be a tuple, list, or Numpy array"
+
+
+
+class TestVelocityMixin(object):
+
+    def setup_method(self, method):
+
+        class SourceTest(Source, VelocityMixin):
+            pass
+
+        self.p = SourceTest()
+
+    def test_default(self):
+        assert self.p.velocity is None
+
+    def test_set_non(self):
+        self.p.velocity = None
+
+    @pytest.mark.parametrize('velocity', [(0., 1., 2.),
+                                          [3., 4., 5.],
+                                          np.array([6., 7., 8.])])
+    def test_set_valid(self, velocity):
+        self.p.velocity = velocity
+
+    @pytest.mark.parametrize('velocity', [(1., 2.),
+                                          [3., 4., 5., 6],
+                                          np.ones(5)])
+    def test_set_invalid_len(self, velocity):
+        with pytest.raises(ValueError) as exc:
+            self.p.velocity = velocity
+        assert exc.value.args[0] == "velocity should be a sequence of 3 values"
+
+    def test_set_invalid_dim(self):
+        with pytest.raises(ValueError) as exc:
+            self.p.velocity = np.ones((3, 2))
+        assert exc.value.args[0] == "velocity should be a 1-D sequence"
+
+    def test_set_invalid_shape(self):
+        with pytest.raises(ValueError) as exc:
+            self.p.velocity = ([0.], [1.,1.], [2.])
+        assert exc.value.args[0] == "velocity should be a sequence of 3 scalar values"
+
+    @pytest.mark.parametrize('velocity', ['a', 1., 4])
+    def test_set_invalid_type(self, velocity):
+        with pytest.raises(ValueError) as exc:
+            self.p.velocity = velocity
+        assert exc.value.args[0] == "velocity should be a tuple, list, or Numpy array"
