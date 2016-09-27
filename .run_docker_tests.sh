e1fe78239ee35945b5225ab86e1157a8fd336e70
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-27 16:47:42 +0100

    Added script to run tests in Docker image

---
+++ .run_docker_tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+GENERATE=0
+REFDIR=reference
+
+while getopts "g" opt; do
+    case "$opt" in
+    g)  GENERATE=1
+        ;;
+    esac
+done
+
+docker info
+
+cat << EOF | docker run -i \
+                        -v ${PWD}:/hyperion_src \
+                        -a stdin -a stdout -a stderr \
+                        a0c658e69938 \
+                        bash || exit $?
+
+set -x
+
+cd /hyperion_src
+./configure
+make serial
+make install
+python setup.py install
+
+if [ $GENERATE == 0 ]; then
+  python setup.py test --enable-bit-level-tests;
+else
+  mkdir -p ${REFDIR};
+  python setup.py test --enable-bit-level-tests --generate-reference=${REFDIR};
+fi
+
+EOF
