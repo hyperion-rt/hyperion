a58d08e5244742ee3ab0e63ecd316b7874f49216
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-02-05 21:51:58 +0000

    Fix setup.py when Numpy isn't present

--- setup.py
+++ setup.py
@@ -12,10 +12,9 @@ from setuptools import setup, Extension, find_packages
 from distutils.command.sdist import sdist
 from distutils.command.build_py import build_py
 
-from numpy import get_include as get_numpy_include
-
 from hyperion.testing.helper import HyperionTest
 from hyperion.version import __version__, __dev__
+
 
 class custom_sdist(sdist):
 
@@ -36,41 +35,48 @@ class custom_sdist(sdist):
             if not self.unstable:
                 open(version_file, 'w').write(content)
 
-numpy_includes = get_numpy_include()
-
 cmdclass = {}
 cmdclass['build_py'] = build_py
 cmdclass['test'] = HyperionTest
 cmdclass['sdist'] = custom_sdist
 
-ext_modules = [Extension("hyperion.util._integrate_core",
-                         ['hyperion/util/_integrate_core.c'],
-                         include_dirs=[numpy_includes],
-                         extra_compile_args=['-Wno-error=declaration-after-statement']),
-               Extension("hyperion.util._interpolate_core",
-                         ['hyperion/util/_interpolate_core.c'],
-                         include_dirs=[numpy_includes],
-                         extra_compile_args=['-Wno-error=declaration-after-statement']),
-               Extension("hyperion.importers._discretize_sph",
-                         ['hyperion/importers/_discretize_sph.c'],
-                         include_dirs=[numpy_includes],
-                         extra_compile_args=['-Wno-error=declaration-after-statement']),
-               Extension("hyperion.grid._voronoi_core",
-                         ['hyperion/grid/_voronoi_core.c',
-                          'hyperion/grid/voropp_wrap.cc',
-                          'hyperion/grid/voro++/c_loops.cc',
-                          'hyperion/grid/voro++/cell.cc',
-                          'hyperion/grid/voro++/common.cc',
-                          'hyperion/grid/voro++/container.cc',
-                          'hyperion/grid/voro++/container_prd.cc',
-                          'hyperion/grid/voro++/pre_container.cc',
-                          'hyperion/grid/voro++/unitcell.cc',
-                          'hyperion/grid/voro++/v_base.cc',
-                          'hyperion/grid/voro++/v_compute.cc',
-                          'hyperion/grid/voro++/wall.cc'],
-                         extra_compile_args = ['-O2', '-Wno-error=declaration-after-statement'],
-                         extra_link_args=['-lstdc++'],
-                         include_dirs=[numpy_includes])]
+if 'egg_info' in sys.argv:
+
+    ext_modules = []
+
+else:
+
+    from numpy import get_include as get_numpy_include
+    numpy_includes = get_numpy_include()
+
+    ext_modules = [Extension("hyperion.util._integrate_core",
+                             ['hyperion/util/_integrate_core.c'],
+                             include_dirs=[numpy_includes],
+                             extra_compile_args=['-Wno-error=declaration-after-statement']),
+                   Extension("hyperion.util._interpolate_core",
+                             ['hyperion/util/_interpolate_core.c'],
+                             include_dirs=[numpy_includes],
+                             extra_compile_args=['-Wno-error=declaration-after-statement']),
+                   Extension("hyperion.importers._discretize_sph",
+                             ['hyperion/importers/_discretize_sph.c'],
+                             include_dirs=[numpy_includes],
+                             extra_compile_args=['-Wno-error=declaration-after-statement']),
+                   Extension("hyperion.grid._voronoi_core",
+                             ['hyperion/grid/_voronoi_core.c',
+                              'hyperion/grid/voropp_wrap.cc',
+                              'hyperion/grid/voro++/c_loops.cc',
+                              'hyperion/grid/voro++/cell.cc',
+                              'hyperion/grid/voro++/common.cc',
+                              'hyperion/grid/voro++/container.cc',
+                              'hyperion/grid/voro++/container_prd.cc',
+                              'hyperion/grid/voro++/pre_container.cc',
+                              'hyperion/grid/voro++/unitcell.cc',
+                              'hyperion/grid/voro++/v_base.cc',
+                              'hyperion/grid/voro++/v_compute.cc',
+                              'hyperion/grid/voro++/wall.cc'],
+                             extra_compile_args = ['-O2', '-Wno-error=declaration-after-statement'],
+                             extra_link_args=['-lstdc++'],
+                             include_dirs=[numpy_includes])]
 
 scripts = ['hyperion', 'hyperion2fits']
 
@@ -90,6 +96,7 @@ setup(name='Hyperion',
       scripts=['scripts/' + x for x in scripts],
       cmdclass=cmdclass,
       ext_modules=ext_modules,
+      setup_requires=['numpy'],
       install_requires=['numpy', 'matplotlib', 'astropy', 'h5py', 'yt'],
       extras_require={'test': ['pytest'], 'docs': ['sphinx', 'numpydoc']}
      )
